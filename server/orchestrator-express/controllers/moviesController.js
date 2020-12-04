df649f8861e6f13a4be3aaf761641b5cd8324ba3
Bintang Wibawa Mukti <bintang.wib@gmail.com>
2020-12-04 16:15:52 +0700

    add redis

--- server/orchestrator-express/controllers/moviesController.js
+++ server/orchestrator-express/controllers/moviesController.js
@@ -1,35 +1,52 @@
 const axios = require('../config/axiosMovies')
+const Redis = require('ioredis')
+const redis = new Redis()
 
 class MoviesController {
-  static getAll (req, res, next) {
-    axios({
-      url: '/movies',
-      method: 'get'
-    })
-      .then(({ data }) => {
-        // console.log(data);
-        res.status(200).json(data)
-      })
-      .catch(err => {
-        res.status(500).json(err)
-      })
+  static async getAll (req, res, next) {
+    try {
+      const dataMovies = await redis.get('dataMovies')
+      if (dataMovies) {
+        // console.log(dataMovies, '<< dataMovies');
+        res.status(200).json(JSON.parse(dataMovies))
+      } else {
+        const response = await axios({
+          url: '/movies',
+          method: 'get'
+        })
+        await redis.set('dataMovies', JSON.stringify(response.data), "EX", 30)
+        setTimeout(() => {
+          res.status(200).json(response.data)
+        }, 2000);
+      }
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 
-  static getById (req, res, next) {
+  static async getById (req, res, next) {
     const id = req.params.id
-    axios({
-      url: `/movies/${id}`,
-      method: 'get'
-    })
-      .then(({ data }) => {
-        res.status(200).json(data)
-      })
-      .catch(err => {
-        res.status(500).json(err)
-      })
+    try {
+      const dataMovie = await redis.get('dataMovie')
+      if (dataMovie) {
+        // console.log(dataMovies, '<< dataMovie');
+        res.status(200).json(JSON.parse(dataMovie))
+      } else {
+        const response = await axios({
+          url: `/movies/${id}`,
+          method: 'get'
+        })
+        await redis.set('dataMovie', JSON.stringify(response.data), "EX", 30)
+        setTimeout(() => {
+          res.status(200).json(response.data)
+        }, 2000);
+      }
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 
-  static create (req, res, next) {
+  static async create (req, res, next) {
     const { title, overview, poster_path, popularity, tags } = req.body
     const payload = {
       title,
@@ -38,21 +55,21 @@ class MoviesController {
       popularity,
       tags
     }
-    axios({
-      url: '/movies',
-      method: 'post',
-      data: payload
-    })
-      .then(({ data }) => {
-        res.status(201).json(data)
+    try {
+      const response = await axios({
+        url: '/movies',
+        method: 'post',
+        data: payload
       })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-      })
+      await redis.del('dataMovies')
+      await redis.del('dataMovie')
+      res.status(201).json(response.data)
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 
-  static editById (req, res, next) {
+  static async editById (req, res, next) {
     const { title, overview, poster_path, popularity, tags } = req.body
     const id = req.params.id
     const payload = {
@@ -62,31 +79,33 @@ class MoviesController {
       popularity,
       tags
     }
-    axios({
-      url: `/movies/${id}`,
-      method: 'put',
-      data: payload
-    })
-      .then(({ data }) => {
-        res.status(200).json(data)
+    try {
+      const response = await axios({
+        url: `/movies/${id}`,
+        method: 'put',
+        data: payload
       })
-      .catch(err => {
-        res.status(500).json(err)
-      })
+      await redis.del('dataMovies')
+      await redis.del('dataMovie')
+      res.status(201).json(response.data)
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 
-  static deletebyId (req, res, next) {
+  static async deletebyId (req, res, next) {
     const id = req.params.id
-    axios({
-      url: `/movies/${id}`,
-      method: 'delete'
-    })
-      .then(({ data }) => {
-        res.status(200).json(data)
+    try {
+      const response = await axios({
+        url: `/movies/${id}`,
+        method: 'delete'
       })
-      .catch(err => {
-        res.status(500).json(err)
-      })
+      await redis.del('dataMovies')
+      await redis.del('dataMovie')
+      res.status(201).json(response.data)
+    } catch (err) {
+      res.status(500).json(err)
+    }
   }
 }
 
